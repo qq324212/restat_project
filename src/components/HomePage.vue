92d669ff90058ddfc101e96b65c828a50b84449b
unknown <arkit5k4@gmail.com>
2025-02-20 12:24:47 +0300

    working on menu

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,7 +1,8 @@
-<template> 
+<template>
+<v-container> 
 <v-card class="mx-auto" elevation="24">
 
-<v-carousel hide-delimiters cycle interval="5000">
+<v-carousel hide-delimiters cycle interval="5000" :show-arrows="false" >
     <v-carousel-item src="https://img.freepik.com/free-psd/food-menu-restaurant-social-media-cover-template_202595-368.jpg?t=st=1739952081~exp=1739955681~hmac=5a19fcc1a806d7a1bd32c2c0f5d41fcdc4527bf8d614e814f5dd258862d49cda&w=1380"  cover></v-carousel-item> 
     <v-carousel-item src="https://img.freepik.com/free-vector/vintage-healthy-food-menu-template-theme_23-2148500576.jpg?t=st=1739952668~exp=1739956268~hmac=c155f12738d361f31fac68d7957c1973d94a9632e415180b8d66f56099683ef5&w=996" cover> </v-carousel-item>
     <v-carousel-item src="https://images.unsplash.com/photo-1480455454781-1af590be2a58?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" cover> </v-carousel-item>
@@ -9,12 +10,17 @@
 </v-carousel>
 
 </v-card>
+</v-container>
 
-<v-container >
-        <v-card class="mx-auto" elevation="24">
+
+ 
+   
+     <v-container>
+        <v-card width="200px" justify-self="center" elevation="24">
+            
             <v-container> 
-            <v-row >
-                <v-col cols="12" md="4">
+            <v-row  >
+                <v-col cols="12" md="5">
                     <v-card>
                         <v-card color="black">
                             <v-card-item>
@@ -30,11 +36,11 @@
                         </v-card>
                         
                     </v-card>
-
+                    
                 </v-col>
-
-                <v-col cols="12" md="4">
-                    <v-card>
+                
+                <v-col cols="12" md="5">
+                <v-card>
                     <v-card color="black">
                     <v-card-item>
                     <v-card-title>Upcoming Events</v-card-title>
@@ -47,10 +53,10 @@
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore voluptatibus!
                     </v-card-text>
                     </v-card>
-                    </v-card>
+                 </v-card>
                 </v-col>
 
-                <v-col cols="12" md="4">
+                <v-col cols="12" md="5">
                     <v-card>
                         <v-card color="black">
                     <v-card-item>
@@ -71,8 +77,57 @@
             </v-row>
         </v-container>
         </v-card>
-    </v-container>
+     </v-container>
+    
+
+
+
+
+
+<v-container>
+    <v-card class="mx-auto" elevation="24" color = "purple">
+        <v-container>
+            <v-row >
+                <v-col cols="12" md="4">
+                    <v-card>
+                        <v-card-item>
+                            <v-card-title>Contact Us</v-card-title>
+                        </v-card-item>
+                        <v-card-text><v-icon icon="mdi-phone" /> 0722123456</v-card-text>
+                        <v-card-text><v-icon icon="mdi-email" /> myrestaurant@example.com</v-card-text>
+                        <v-card-text>Operating Hours: 10.00am - 10.00pm</v-card-text>
+                    </v-card>
+                </v-col>
+
+                    <v-col cols="12" md="4">
+                    <v-card>
+                        <v-card-item>
+                        <v-card-title>Find us on our socials</v-card-title>
+
+                        </v-card-item>
+                        <v-card-text><v-icon icon="mdi-instagram" /> myrestaurant</v-card-text>
+                        <v-card-text><v-icon icon="mdi-twitter" /> myrestaurant</v-card-text>
+                        <v-card-text><v-icon icon="mdi-facebook" /> myrestaurant</v-card-text>
+                    </v-card>
+                </v-col>
+                <v-col cols="12" md="4">
+                    <v-card>
+                        <v-card-item>
+                        <v-card-title>Our Restaurant Locations</v-card-title>
+                        </v-card-item>
+                        <v-card-text>Madaraka</v-card-text>
+                        <v-card-text>CBD</v-card-text>
+                        <v-card-text>Langata</v-card-text>
+                    </v-card>
+                </v-col>
+            </v-row>
+        </v-container>
+    </v-card>
+</v-container>
 </template>
+
+
+
 
 <script setup>
 </script>
--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,6 +1,58 @@
 <template> 
-    <h1>Menu</h1>
-    </template>
-    
-    <script setup>
-    </script>
+<v-container>
+        <v-card color="blue" >
+            <v-card-title>
+                <div class="text-h3">Our Menu</div>
+            </v-card-title>
+        </v-card>
+    </v-container>
+  <v-container fluid="true">
+     <v-card elevation="24">
+         
+         <v-container> 
+         <v-row  >
+             <v-col cols="12" md="5" v-for="item in menuItems" :key=item>
+                 <v-card>
+                     <v-card color="black">
+                         <v-card-item>
+                             <v-card-title>{{ item.name }}</v-card-title>
+
+                             <v-card-subtitle>{{ item.description }}</v-card-subtitle>
+                         </v-card-item>
+                         <v-img height="200px" src="https://img.freepik.com/free-vector/vintage-healthy-food-menu-template-theme_23-2148500576.jpg?t=st=1739952668~exp=1739956268~hmac=c155f12738d361f31fac68d7957c1973d94a9632e415180b8d66f56099683ef5&w=996" cover ></v-img>
+
+                         <v-card-text>
+                         {{ item.price }}
+                         </v-card-text>
+                     </v-card>
+                     
+                 </v-card>
+                 
+             </v-col>
+         </v-row>
+         </v-container>
+     </v-card>
+  </v-container>  
+</template>
+
+
+<script setup>
+const menuItems = [
+    {
+        name: "Chips",
+        price: 200,
+        description: "Best chips in town",
+        size: "Large",
+        offers: "None",
+        image: "https://unsplash.com/photos/a-basket-of-french-fries-sitting-on-top-of-a-wooden-table-ChXHveqrb28?utm_content=creditShareLink&utm_medium=referral&utm_source=unsplash"
+    },
+    {
+        name: "Sausage",
+        price: 50,
+        description: "Best sausages in town",
+        size: "None",
+        offers: "None",
+        image: "https://unsplash.com/photos/sausage-on-black-round-pan-cSxpCQrRlo8"
+    }
+]
+</script>
